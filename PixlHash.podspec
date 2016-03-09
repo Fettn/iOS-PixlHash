2fbfc9dc8ee76df884ade9622caf97936808b951
Fettn <fettn@baerinteractive.de>
2016-03-09 23:21:10 +0100

    Fixing the memory bug by switching to drawing on a ImageContext. Increasing the Version number to 0.2.0 Lowering the deployment target to 7.0

--- PixlHash.podspec
+++ PixlHash.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = "PixlHash"
-  s.version          = "0.1.0"
+  s.version          = "0.2.0"
   s.summary          = "Create a pixel avatar from any input string."
 
 # This description is used to generate tags and improve search results.
--- Pod/Classes/PixlHash.m
+++ Pod/Classes/PixlHash.m
@@ -52,47 +52,54 @@
     uint8_t digest[CC_SHA256_DIGEST_LENGTH] = {0};
     CC_SHA256(inputData.bytes, (int)inputData.length, digest);
     NSData *output = [NSData dataWithBytes:digest length:CC_SHA256_DIGEST_LENGTH];
-    uint8_t *hash = calloc(self.tileWidth * WIDTH * self.tileHeight * HEIGHT * 4, sizeof(uint8_t));
-    [self createHash: output withHash: hash];
-    CGDataProviderRef provider = CGDataProviderCreateWithData(NULL, hash, self.tileWidth * WIDTH * self.tileHeight * HEIGHT * 4, NULL);
-    
-    CGColorSpaceRef colorSpaceRef = CGColorSpaceCreateDeviceRGB();
-    CGBitmapInfo bitmapInfo = kCGBitmapByteOrderDefault | kCGImageAlphaFirst;
-    CGColorRenderingIntent renderingIntent = kCGRenderingIntentDefault;
-    CGImageRef imageRef = CGImageCreate(self.tileWidth * WIDTH, self.tileHeight * HEIGHT, 8, 32, 4 * self.tileWidth * WIDTH, colorSpaceRef, bitmapInfo, provider, NULL, NO, renderingIntent);
-    UIImage* image = [UIImage imageWithCGImage:imageRef];
-    CGDataProviderRelease(provider);
-    CGColorSpaceRelease(colorSpaceRef);
-    CGImageRelease(imageRef);
-    free(hash);
-    return image;
+
+    CGSize newSize = CGSizeMake(self.tileWidth * WIDTH, self.tileHeight * HEIGHT);
+    UIGraphicsBeginImageContext(newSize);
+
+    [self createHash: output];
+
+    UIImage* outputImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return outputImage;
 }
 
 #pragma mark Private
 
-- (NSInteger)getColor:(const char *)input fromStartValue:(NSInteger)start {
-    NSInteger ret = 0xFF;
-    for (NSInteger offset = 0; offset < 3; offset++) {
-        ret *= 256;
-        ret += input[start + offset];
+- (UIColor *)getColor:(const char *)input fromStartValue:(NSInteger)start {
+    NSInteger a = 1.0;
+    CGFloat r = ((CGFloat) input[start] / 0xFF);
+    if (r < 0) {
+        r = 1.0 + r;
     }
-    return ret;
+    CGFloat g = ((CGFloat) input[start + 1] / 0xFF);
+    if (g < 0) {
+        g = 1.0 + g;
+    }
+    CGFloat b = ((CGFloat) input[start + 2] / 0xFF);
+    if (b < 0) {
+        b = 1.0 + b;
+    }
+    return [UIColor colorWithRed:r green:g blue:b alpha:a];
 }
 
-- (NSInteger)calculateColorDistanceBetweenFirstColor:(NSInteger)color1 andSecondColor:(NSInteger)color2 {
-    NSInteger sum = 0;
-    for (NSInteger i = 0; i < COLOR_SIZE; i++) {
-        NSInteger diff = color1 % 256 - color2 % 256;
-        sum += diff * diff;
-        color1 /= 256;
-        color2 /= 256;
-    }
-    return sum;
+- (NSInteger)calculateColorDistanceBetweenFirstColor:(UIColor *)color1 andSecondColor:(UIColor *)color2 {
+    CGFloat red1, red2;
+    CGFloat green1, green2;
+    CGFloat blue1, blue2;
+    CGFloat alpha1, alpha2;
+    [color1 getRed:&red1 green:&green1 blue:&blue1 alpha:&alpha1];
+    [color2 getRed:&red2 green:&green2 blue:&blue2 alpha:&alpha2];
+
+    NSInteger deltaRed = (NSInteger) (red1 * 0xFF - red2 * 0xFF);
+    NSInteger deltaGreen = (NSInteger) (green1 * 0xFF - green2 * 0xFF);
+    NSInteger deltaBlue = (NSInteger) (blue1 * 0xFF - blue2 * 0xFF);
+    NSInteger deltaAlpha = (NSInteger) (alpha1 * 0xFF - alpha2 * 0xFF);
+    return deltaRed * deltaRed + deltaGreen * deltaGreen + deltaBlue * deltaBlue + deltaAlpha * deltaAlpha;
 }
 
-- (void)pickColors:(NSData *)input selectFirstColor:(NSInteger *)color1 secondColor:(NSInteger *)color2 {
-    *color1 = 0xFFFFFF;
-    *color2 = 0;
+- (void)pickColors:(NSData *)input selectFirstColor:(UIColor **)color1 secondColor:(UIColor **)color2 {
+    *color1 = [UIColor blackColor];
+    *color2 = [UIColor whiteColor];
     for (NSInteger start = 0; start < input.length - 6; start++) {
         *color1 = [self getColor:input.bytes fromStartValue:start];
         *color2 = [self getColor:input.bytes fromStartValue:start + 3];
@@ -102,26 +109,25 @@
     }
 }
 
-- (void) createHash:(NSData *)input withHash:(uint8_t *)hash {
-    NSInteger color1;
-    NSInteger color2;
+- (void)createHash:(NSData *)input {
+    UIColor* color1;
+    UIColor* color2;
     [self pickColors: input selectFirstColor: &color1 secondColor: &color2];
-    NSInteger len = WIDTH * self.tileWidth * HEIGHT * self.tileHeight;
-    for (NSInteger position = 0; position < len; position++) {
-        [self setColor:hash position:position * 4 color:color2];
-    }
+    [color2 setFill];
+    UIRectFill(CGRectMake(0, 0, self.tileWidth * WIDTH, self.tileHeight * HEIGHT));
+    [color1 setFill];
     for (NSInteger position = 0; position < (input.length - 1) * 4; position++) {
         NSInteger x;
         NSInteger y;
         NSInteger set;
         [self getPixelData: input atPosition: position outputX: &x outputY: &y outputSet: &set];
         if (set) {
-            [self drawRect: hash xCoord: x yCoord: y withColor: color1];
+            [self drawRectAtXCoord: x AndYCoord: y];
         }
     }
 }
 
-- (void) getPixelData:(NSData *)input atPosition:(NSInteger)position outputX:(NSInteger *)x outputY:(NSInteger *)y outputSet:(NSInteger *)set {
+- (void)getPixelData:(NSData *)input atPosition:(NSInteger)position outputX:(NSInteger *)x outputY:(NSInteger *)y outputSet:(NSInteger *)set {
     NSInteger offset = position / (input.length - 1) * 2;
     NSInteger start = position % (input.length - 1);
     const uint8_t* bytes = [input bytes];
@@ -132,23 +138,9 @@
     *y = coordinateData / 8;
 }
 
-- (void) drawRect:(uint8_t *)hash xCoord:(NSInteger)x yCoord:(NSInteger)y withColor:(NSInteger)color {
-    for (NSInteger deltaY = 0; deltaY < self.tileHeight; deltaY++) {
-        for (NSInteger deltaX = 0; deltaX < self.tileWidth; deltaX++) {
-            NSInteger pos1 = ((y * self.tileHeight + deltaY) * (WIDTH * self.tileWidth) + (x * self.tileWidth) + deltaX);
-            NSInteger pos2 = ((y * self.tileHeight + deltaY) * (WIDTH * self.tileWidth) + ((WIDTH - x - 1) * self.tileWidth) + deltaX);
-            [self setColor: hash position:pos1 * 4 color: color];
-            [self setColor: hash position:pos2 * 4 color: color];
-        }
-    }
-}
-
-- (void) setColor:(uint8_t *)field position:(NSInteger)position color:(NSInteger)color {
-    for (NSInteger b = 3; b >= 0; b--) {
-        field[position + b] = color % 256;
-        field[position + b] = color % 256;
-        color /= 256;
-    }
+- (void)drawRectAtXCoord:(NSInteger)x AndYCoord:(NSInteger)y {
+    UIRectFill(CGRectMake(x * self.tileWidth, y * self.tileHeight, self.tileWidth, self.tileHeight));
+    UIRectFill(CGRectMake((WIDTH - x - 1) * self.tileWidth, y * self.tileHeight, self.tileWidth, self.tileHeight));
 }
 
 @end
